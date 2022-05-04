6dc370ac01b1475c231ec6ba511b73f34d902d77
Mohammed-Salama <mohammedsalama1617@gmail.com>
2022-05-04 21:02:24 +0200

    follow line

--- Fast line follower/FLF/FLF.ino
+++ Fast line follower/FLF/FLF.ino
@@ -9,29 +9,38 @@ const int RForward = 5;
 const int RightBachward = 4;
 const int LForward = 7;
 const int LeftBachward = 6;
-
-
-
+byte prev = 0;
+byte curr = 0;
+int idx = 0;
+int mode = 1;
 const int minDif = 5;
 const int thr = 700;
 int turnSpeed = 50;
 int straightSpeed = 200;
 
-
- void followLine(){
+byte dir[1000];
+short times[1000];
+byte  followLine(){
     //Reading sensors, if HIGH (BLACK Line) or LOW (No Line), if black line on Sensor Right, we go Right and contrary
     int valueLeft= analogRead(IRSensorL);
     int valueRight = analogRead(IRSensorR); 
     int valueStraight = analogRead(IRSensorS);
     if(valueLeft > thr && valueLeft-valueStraight > minDif){     
       go_Left();      
-    } else if (valueRight > thr && valueRight-valueStraight > minDif){
-      go_Right();      
-    } else{
+      return 1;
+    } 
+    else if (valueRight > thr && valueRight-valueStraight > minDif){
+      go_Right();    
+      return 2;
+    } 
+    else {
       go_Advance();
+      return 3;
     }
 }
+void fastFollowLine(){
 
+}
 void set_Motorspeed(int valR , int valL ){ 
   analogWrite(LeftMotorPWM, valL);
   analogWrite(RightMotorPWM, valR);
@@ -83,5 +92,20 @@ void setup() {
 }
 
 void loop() {
-    followLine();
+    if (mode==1){
+      curr = followLine();
+      if (curr != prev){
+        dir[idx] = curr;
+        times[idx]=1;
+        idx ++;
+      }
+      else{
+        times[idx]++;
+      }
+      prev = curr;
+    }
+    else {
+      fastFollowLine();
+    }
+    delay(500);
 }
